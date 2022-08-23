c748ed27c2da4b6287623b8eb3a152859edb525c
Keshav <skeshav162@gmail.com>
2022-08-23 14:06:52 +0530

    paytml clone half done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        "primary" : "rgb(1,186,243)",
+        "secondary": "#0F4B8B"
+      }
+    },
+  },
+  plugins: [],
+}
